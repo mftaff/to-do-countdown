25e0843a7a65ca3a83ef5c1ac2af1dc39388ca7f
Meir Taffel <artgardening@gmail.com>
2017-05-04 15:04:10 +0000

    add jquery to toggle expired tasks

---
+++ app/assets/javascripts/toggleExpired.js
@@ -0,0 +1,11 @@
+$( document ).ready(function(){
+    $('#toggle-expired').click(function(){
+        $('#expired-tasks').toggle(100, function(){
+            if($('#toggle-expired').text() === "Show Expired Tasks") {
+                $('#toggle-expired').text('Hide Expired Tasks');
+            } else {
+                $('#toggle-expired').text('Show Expired Tasks');
+            }
+        });       
+    });   
+});
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,15 +1,17 @@
 module TasksHelper
-    def time_factor(time_left, created_at)
+    def time_factor(time_left, task)
         t = time_left.split.last
         class_name = ""
         
-        if created_at > Time.now - 7.days
+        if task.created_at > Time.now - 7.days
             if t == "day" || t == "hours"
                 class_name = "bg-warning" 
             elsif t == "hour" || t == "minute" || t == "minutes" 
                 class_name = "bg-danger"
             end
             "<td class='#{class_name}'>#{time_left}</td>".html_safe
+        elsif task.expired == false
+            "<td class='bg-danger'>TIME IS ALMOST UP!</td>".html_safe
         else
             "<td class='bg-danger'>EXPIRED!</td>".html_safe
         end
